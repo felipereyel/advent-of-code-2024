bd0ba0997908db69f9a69416167291854a5b6387
felipereyel <felipe.reyel@gmail.com>
2024-12-01 23:04:48 -0300

    add go solution

--- problems/01/first/main.go
+++ problems/01/first/main.go
@@ -1,5 +1,55 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
 
+func dist(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
+
+func main() {
+	file := os.Args[1]
+
+	bcontent, err := os.ReadFile(file)
+	if err != nil {
+		panic("Failed to open file")
+	}
+
+	scontent := string(bcontent)
+
+	lines := strings.Split(scontent, "\n")
+	nlines := len(lines)
+	lefts := make([]int, nlines)
+	rights := make([]int, nlines)
+
+	for i, line := range lines {
+		parts := strings.Split(line, "   ")
+
+		lefts[i], err = strconv.Atoi(parts[0])
+		if err != nil {
+			panic("bad left value")
+		}
+		rights[i], err = strconv.Atoi(parts[1])
+		if err != nil {
+			panic("bad right value")
+		}
+	}
+
+	slices.Sort(lefts)
+	slices.Sort(rights)
+
+	total := 0
+	for i := range nlines {
+		total += dist(lefts[i], rights[i])
+	}
+
+	fmt.Println(total)
 }
