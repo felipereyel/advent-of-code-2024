7eb8d9d4e08551bc84c0f5e871522d9b67acdd4c
felipereyel <felipe.reyel@gmail.com>
2024-12-03 23:48:09 -0300

    add go for 3.1

---
+++ problems/03/first/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func mustAtoi(s string) int {
+	r, err := strconv.Atoi(s)
+
+	if err != nil {
+		panic("bad atoi input")
+	}
+
+	return r
+}
+
+func mul(s string) int {
+	total := 0
+	expr := regexp.MustCompile("mul\\((\\d{1,3},\\d{1,3})\\)")
+	for _, match := range expr.FindAllStringSubmatch(s, -1) {
+		parts := strings.Split(match[1], ",")
+		total += mustAtoi(parts[0]) * mustAtoi(parts[1])
+	}
+
+	return total
+}
+
+func main() {
+	file := os.Args[1]
+
+	bcontent, err := os.ReadFile(file)
+	if err != nil {
+		panic("Failed to open file")
+	}
+
+	scontent := string(bcontent)
+
+	total := 0
+	total += mul(scontent)
+
+	fmt.Println(total)
+}
